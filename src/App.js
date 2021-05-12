fa384c5b6757a785bcfe1b95dc20c110fe4db977
Marcos-111 <escalzomd@gmail.com>
2021-05-12 12:56:00 -0300

    Component Header created | layout of generator created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Generator from "./generator/generator"
+import Header from "./header/header"
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Generator/>
     </div>
   );
