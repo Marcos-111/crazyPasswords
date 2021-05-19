e5ba64b217eb6d96345a2712f6042294e67180a1
Gonzalo Simon <aguilarsimon01@gmail.com>
2021-05-19 12:22:32 -0300

    I'm testing the register action

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const mysql = require("mysql")
-
 const app = express();
-
 app.use(express.json());
 
 const db = mysql.createConnection({
   user:"root",
   host:"localhost",
   password:"",
-  database:"",
+  database:"LoginSystem",
 });
+
+
+app.post("/register", (req, res) => {
+
+
+  const username = req.body.username;
+  const password = req.body.password;
+  db.query(
+    "INSERT INTO users (username, passwords) VALUES (?,?)",
+    [username, password],
+    (err, result) => {
+      console.log(err)
+    }
+  );
+});
+
+app.post('/login', (req, res ) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  db.query(
+    "SELECT * FROM users WHERE username = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send(err: err)
+      }
+      else {
+        if (result) {
+          res.send(result)
+        } else {
+          res.send({message: "Wrong username/password"})
+        }
+      }
+    }
+  );
+});
+app.listen(3001, () => {
+  console.log("running server")
+})
